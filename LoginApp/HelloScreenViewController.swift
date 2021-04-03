cefc9350ea481dde16777b50422583418026d28e
dcodocdcodco <snoozelight0@gmail.com>
2021-04-03 04:08:19 +0300

    working version

--- LoginApp/HelloScreenViewController.swift
+++ LoginApp/HelloScreenViewController.swift
@@ -9,21 +9,13 @@ import UIKit
 
 class HelloScreenViewController: UIViewController {
 
-    @IBOutlet weak var logOutButton: UIButton!
-    
     @IBOutlet weak var welcomeText: UILabel!
     
     var welcomeTextAgent: String! 
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        welcomeText.text = "Welcome, " + welcomeTextAgent + "!" // полностью согласен , что это выглядит ужасно , но по-другому не додумался
+        welcomeText.text = "Welcome, " + welcomeTextAgent + "!" // 
     }
-    
-    @IBAction func logOutAction() {
-        dismiss(animated: true)
-    }
-    
-    
+
 }
--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginScreenViewController: UIViewController {
     
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
-    @IBOutlet weak var logInButton: UIButton!
+    private let userName = "User"
+    private let password = "Password"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,43 +21,44 @@ class ViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let helloScreenVC = segue.destination as? HelloScreenViewController else { return }
-        helloScreenVC.welcomeTextAgent = usernameField.text
+        helloScreenVC.welcomeTextAgent = userName
     }
     
-    
-    // c log in action у меня глобальная проблема , я провел полночи в гугле , но мне это не помогло , как бы я не менял местами passwordfield и usernamefield , ставил между ними логический оператор , как бы не крутил-вертел весь код , все равно вводя в приложении верное значение для только для поля username (не вводя ничего в password) приложение пропускает дальше / при этом если я убераю упоминание про username , приложение начинает возбуждаться на заполнение/незаполнение поля с паролем . вторую домашнюю подряд у меня ничего не выходит , видимо я безнадежен
     @IBAction func logInAction() {
-        if passwordField.text != "Pass" && usernameField.text != "User" {
-                showAlert(title: "Wrong !", andMessage: "Your Username or Password incorrect")
+        if usernameField.text != userName || passwordField.text != password {
+            showAlert(
+                title: "Wrong !",
+                andMessage: "Your Username or Password incorrect"
+                )
+            return
         }
     }
     
-    
-    @IBAction func forgotNameAlertAction() {
-        forgotAlert(title: "Don't Worry!", andMessage: "Your username is 'User'")
+    @IBAction func forgotAlertAction(_ sender: UIButton) {
+        sender.tag == 0
+            ? showAlert(
+                title: "Forgot your username?",
+                andMessage: "Your username is 'User'"
+            )
+            : showAlert(
+                title: "Forgot your password?",
+                andMessage: "Your password is 'Password'"
+            )
     }
     
-    @IBAction func forgotPasswordAlertAction() {
-        forgotAlert(title: "Don't Worry!", andMessage: "Your password is 'Pass'")
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+        usernameField.text = ""
+        passwordField.text = ""
     }
-    
 }
 
 // MARK: - Alert Controllers
-extension ViewController {
+extension LoginScreenViewController {
     private func showAlert(title: String, andMessage: String) {
         let alert = UIAlertController(title: title, message: andMessage, preferredStyle: .alert)
-        let alertButton = UIAlertAction(title: "OK", style: .cancel) { _ in
-            self.usernameField.text = ""
+        let alertButton = UIAlertAction(title: "OK", style: .default) { _ in
             self.passwordField.text = ""
         }
-        alert.addAction(alertButton)
-        present(alert, animated: true)
-    }
-    
-    private func forgotAlert(title: String, andMessage: String) {
-        let alert = UIAlertController(title: title, message: andMessage, preferredStyle: .actionSheet)
-        let alertButton = UIAlertAction(title: "OK", style: .cancel)
         alert.addAction(alertButton)
         present(alert, animated: true)
     }
